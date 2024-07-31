d21b57133664855dac2f74cab8cd28f61e22374d
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2024-07-31 10:27:13 +0300

    Update DateUtil.h

--- DateUtil.h
+++ DateUtil.h
@@ -5,7 +5,15 @@
 #ifndef DATEUTIL_H
 #define DATEUTIL_H
 #include <stdbool.h>
-int dayOfMonths[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 bool isLeapYear(int year);
 bool isValidDate(int day, int month, int year);
+int getDayOfYear(int day, int month, int year);
+int getTotalDays(int year, int totalDays);
+int getDayOfWeek(int day, int month, int year);
+void printDateByDayOfWeekTR(int day, int month, int year, int dayOfWeek);
+void printDateByDayOfWeekEN(int day, int month, int year, int dayOfWeek);
+void printDateByDayOfWeekDE(int day, int month, int year, int dayOfWeek);
+void printDateTR(int day, int month, int year);
+void printDateEN(int day, int month, int year);
+void printDateDE(int day, int month, int year);
 #endif //DATEUTIL_H
